36448a9d972a676ea978403407732f067d686112
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-04-12 22:52:03 -0700

    wip sql

--- migrations.py
+++ migrations.py
@@ -1,12 +1,51 @@
-# migrations.py is for building your database
+async def m001_initial_forms(db):
 
-# async def m001_initial(db):
-#    await db.execute(
-#        f"""
-#        CREATE TABLE forms.forms (
-#            id TEXT PRIMARY KEY,
-#            wallet TEXT NOT NULL,
-#            time TIMESTAMP NOT NULL DEFAULT {db.timestamp_now}
-#        );
-#    """
-#    )
+    # STATUS COLUMN OPTIONS: 'draft', 'open', 'paid', 'canceled'
+
+    await db.execute(
+        f"""
+       CREATE TABLE forms.forms (
+           id TEXT PRIMARY KEY,
+           wallet TEXT NOT NULL,
+
+           status TEXT NOT NULL DEFAULT 'draft',
+
+           currency TEXT NOT NULL,
+
+           form_name TEXT DEFAULT NULL,
+           custom_css TEXT DEFAULT NULL,
+
+
+           time TIMESTAMP NOT NULL DEFAULT {db.timestamp_now}
+       );
+   """
+    )
+
+    await db.execute(
+        f"""
+       CREATE TABLE forms.forms_items (
+           id TEXT PRIMARY KEY,
+           forms_id TEXT NOT NULL,
+
+           description TEXT NOT NULL,
+           field_type TEXT NOT NULL,
+
+           FOREIGN KEY(forms_id) REFERENCES {db.references_schema}forms(id)
+        );
+   """
+    )
+
+    await db.execute(
+        f"""
+       CREATE TABLE forms.payments (
+           id TEXT PRIMARY KEY,
+           forms_id TEXT NOT NULL,
+
+           amount {db.big_int} NOT NULL,
+
+           time TIMESTAMP NOT NULL DEFAULT {db.timestamp_now},
+
+           FOREIGN KEY(forms_id) REFERENCES {db.references_schema}forms(id)
+       );
+   """
+    )
--- models.py
+++ models.py
@@ -1,5 +1,94 @@
-# from pydantic import BaseModel
+from enum import Enum
+from sqlite3 import Row
+from typing import List, Optional
 
-# class Example(BaseModel):
-#    id: str
-#    wallet: str
+from fastapi import Query
+from pydantic import BaseModel
+
+
+class FormStatusEnum(str, Enum):
+    draft = "draft"
+    open = "open"
+    paid = "paid"
+    canceled = "canceled"
+
+
+class CreateFormItemData(BaseModel):
+    description: str
+    amount: float = Query(..., ge=0.01)
+
+
+class CreateFormData(BaseModel):
+    status: FormStatusEnum = FormStatusEnum.draft
+    currency: str
+    form_name: Optional[str]
+    custom_css: Optional[str]
+    amount: float = Query(..., ge=0.01)
+    items: List[CreateFormItemData]
+
+    class Config:
+        use_enum_values = True
+
+
+class UpdateFormItemData(BaseModel):
+    id: Optional[str]
+    description: str
+    field_type: str
+
+
+class UpdateFormData(BaseModel):
+    id: str
+    wallet: str
+    status: FormStatusEnum = FormStatusEnum.draft
+    currency: str
+    form_name: Optional[str]
+    custom_css: Optional[str]
+    amount: float = Query(..., ge=0.01)
+    items: List[UpdateFormItemData]
+
+
+class Form(BaseModel):
+    id: str
+    wallet: str
+    status: FormStatusEnum = FormStatusEnum.draft
+    currency: str
+    form_name: Optional[str]
+    custom_css: Optional[str]
+    time: int
+
+    class Config:
+        use_enum_values = True
+
+    @classmethod
+    def from_row(cls, row: Row) -> "Form":
+        return cls(**dict(row))
+
+
+class FormItem(BaseModel):
+    id: str
+    Form_id: str
+    description: str
+    field_type: str
+
+    class Config:
+        orm_mode = True
+
+    @classmethod
+    def from_row(cls, row: Row) -> "FormItem":
+        return cls(**dict(row))
+
+
+class Payment(BaseModel):
+    id: str
+    Form_id: str
+    amount: int
+    time: int
+
+    @classmethod
+    def from_row(cls, row: Row) -> "Payment":
+        return cls(**dict(row))
+
+
+class CreatePaymentData(BaseModel):
+    Form_id: str
+    amount: int
